3981c68b2fc644178bfabcc334ec71aa984691ae
V.Thanaledsumy <thanamvisva99@gmail.com>
2024-12-01 17:09:52 +0530

    Employee section

---
+++ src/app/employees/employee-cv/employee-cv.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router, RouterOutlet,ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-cv',
+  imports: [RouterOutlet],
+  templateUrl: './employee-cv.component.html',
+  styleUrl: './employee-cv.component.css'
+})
+export class EmployeeCvComponent {
+
+
+  
+}
