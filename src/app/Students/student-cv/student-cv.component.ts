bc3bf1107dd12eb7b9d0920c4182e7cafd3a8690
V.Thanaledsumy <thanamvisva99@gmail.com>
2024-12-03 19:06:18 +0530

    Load data

--- src/app/Students/student-cv/student-cv.component.ts
+++ src/app/Students/student-cv/student-cv.component.ts
@@ -15,6 +15,11 @@ export class StudentCvComponent implements OnInit {
   student: any;
   parentsForm!: FormGroup;
   addressForm!: FormGroup;
+  olQualificationForm!: FormGroup;
+  alQualificationForm!:FormGroup;
+  higherStudyForm!: FormGroup;
+  experienceForm!: FormGroup;
+
 
   studentData: any = {};
   isEditingAddress: boolean = false;  
@@ -68,6 +73,51 @@ export class StudentCvComponent implements OnInit {
       postalCode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
       country: ['', Validators.required],
     });
+
+    this.olQualificationForm = this.fb.group({
+      indexNo: ['', Validators.required],
+      year: ['', Validators.required],
+      school: ['', Validators.required],
+      tamil: ['', Validators.required],
+      science: ['', Validators.required],
+      maths: ['', Validators.required],
+      religion: ['', Validators.required],
+      english: ['', Validators.required],
+      history: ['', Validators.required],
+      basket1: ['', Validators.required],
+      basket2: ['', Validators.required],
+      basket3: ['', Validators.required]
+    });
+
+    this.alQualificationForm = this.fb.group({
+      indexNo: ['', Validators.required],
+      year: ['', Validators.required],
+      school: ['', Validators.required],
+      stream: ['', Validators.required],
+      subject1: ['', Validators.required],
+      subject2: ['', Validators.required],
+      subject3: ['', Validators.required],
+      generalEnglish: ['', Validators.required],
+      generalKnowledge: ['', Validators.required],
+      git: ['', Validators.required]
+    });
+    this.higherStudyForm = this.fb.group({
+      stream: ['', Validators.required],
+      year: ['', Validators.required],
+      duration: ['', Validators.required],
+      description: ['', Validators.required],
+      institute: ['', Validators.required],
+      grade: ['', Validators.required]
+    });
+
+    this.experienceForm = this.fb.group({
+      companyName: ['', Validators.required],
+      position: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  
   }
    // Save the parent data to localStorage
    saveData(form: FormGroup, key: string) {
@@ -92,6 +142,30 @@ export class StudentCvComponent implements OnInit {
       alert('Please fill all required fields correctly.');
     }
   }
+  saveQualification() {
+    if (this.olQualificationForm.valid) {
+      const qualificationData = this.olQualificationForm.value;
+      this.studentData.olQualification = qualificationData; 
+      localStorage.setItem('studentData', JSON.stringify(this.studentData));
+      alert('O-Level qualification saved successfully!');
+      this.olQualificationForm.reset();
+    } else {
+      alert('Please fill all required fields correctly.');
+    }
+  }
+  
+  saveAlQualification() {
+    
+    if (this.alQualificationForm.valid) {
+      this.studentData.alQualification = this.alQualificationForm.value;
+      localStorage.setItem('studentData', JSON.stringify(this.studentData));
+      alert('A-Level qualification saved successfully!');
+      this.alQualificationForm.reset();
+    } else {
+      alert('Please fill all required fields correctly.');
+    }
+  }
+  
 
   editAddress() {
     if (this.studentData.address) {
