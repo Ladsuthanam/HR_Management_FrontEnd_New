d08c691f388ef0c2a794e742718e56646c0fd608
Sopraj1120 <manchukansopraj@gmail.com>
2024-12-05 11:23:31 +0530

    add stdent cv remove routerlink

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),importProvidersFrom(BrowserAnimationsModule), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),importProvidersFrom(BrowserAnimationsModule, HttpClientModule), 
+     provideAnimationsAsync()]
 };
